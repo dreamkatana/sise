de792f5cf9dcdeb631221b71e0329afdbd15e588
Joao Eduardo <joaoedu@gmail.com>
2025-09-15 16:31:17 -0300

    Remove arquivo sensível do repositório

--- config/config.py
+++
@@ -1,6 +0,0 @@
-# Configurações do Flask e Keycloak
-SECRET_KEY = 'troque-por-uma-chave-segura'
-KEYCLOAK_SERVER_URL = 'https://autenticacaocentral.unicamp.br/auth/'
-KEYCLOAK_CLIENT_ID = 'educorp-apoios-prod'
-KEYCLOAK_REALM = 'unicamp-auth'
-KEYCLOAK_CLIENT_SECRET = 'cae85610-cafb-4c21-b8b2-19578fb8007d'
